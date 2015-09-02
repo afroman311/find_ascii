1feb6f5731da38ac28e6c654915e2c9ec80522a2
afroman311 <boettgers@my.wccc.edu>
2015-09-02 17:03:02 -0400

    Homework assignment 1

---
+++ Find_ascii.java
@@ -0,0 +1,21 @@
+
+package find_ascii;
+
+public class Find_ascii {
+
+    public static void main(String[] args) {
+        
+        int sum;
+        
+        char char1 = 'D';
+        char char2 = 'o';
+        char char3 = 'g';
+        char char4 = 'm';
+        char char5 = 'e';
+        char char6 = 'a';
+        char char7 = 't';
+        System.out.println(char1 + char2 + char3 + char4 + char5 +char6 + char7);
+        
+    }
+    
+}
